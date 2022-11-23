fa27434f80df8a88b26d9e72234f5c3456519bc3
Vadim <sg_vadim@mail.ru>
2022-11-23 11:03:36 +0300

    Сделали DZ1

---
+++ DZ1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine(value:"Введите первое число ");
+int num1 = Convert.ToInt32 (value: Console.ReadLine());
+Console.WriteLine(value:"Введите второе число ");
+int num2 = Convert.ToInt32 (value: Console.ReadLine());
+
+    if(num1 > num2)
+{        
+    Console.Write("Больше число ");
+    Console.WriteLine(num1);
+}
+    else 
+{
+    Console.Write("Больше число ");
+    Console.WriteLine(num2);
+}
