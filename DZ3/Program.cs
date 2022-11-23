509e1bd20690a4ff80accbd82eac991627c2115b
Vadim <sg_vadim@mail.ru>
2022-11-23 22:39:26 +0300

    DZ32

---
+++ DZ3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine(value:"Является число четным?");
+Console.WriteLine(value:"Введите число ");
+int num1 = Convert.ToInt32 (value: Console.ReadLine());
+
+    if(num1 % 2 == 0)
+{        
+    Console.WriteLine("Да");
+    
+}
+    else 
+{
+    Console.WriteLine("Нет");
+    
+}
+    
